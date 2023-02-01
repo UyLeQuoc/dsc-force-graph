7fe35926d15d0be3fffe830844fa83bc840f92e5
satoshiuy <lequocuyit@gmail.com>
2023-02-01 14:43:46 +0700

    add graph workspace

--- components/GraphCard.tsx
+++ components/GraphCard.tsx
@@ -1,24 +1,85 @@
-import { Avatar, Card, List, Skeleton } from 'antd';
+import { Avatar, Button, Card, Input, List, message, Modal, Popconfirm, Skeleton, Space, Typography } from 'antd';
 import {SettingOutlined, EditOutlined, EllipsisOutlined} from '@ant-design/icons';
 import { useState } from 'react';
 import Link from 'next/link';
 
-function GraphCard({id, title, owner, lastModified} : {id:string, title: string, owner: string, lastModified: any}) : JSX.Element {
+type IProps = {
+  id: string;
+  title: string;
+  owner: string;
+  lastModified: any;
+  handleRenameGraphTitle: (id: string, title: string) => void;
+}
+function GraphCard({id, title, owner, lastModified, handleRenameGraphTitle, handleDeleteGraph}:IProps) : JSX.Element {
   const [loading, setLoading] = useState(false);
+  const [graphTitle, setGraphTitle] = useState<string>(title);
+  const [newTitle, setNewTitle] = useState<string>(title);
 
-  const onChange = (checked: boolean) => {
-    setLoading(!checked);
+  const [open, setOpen] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleOk = () => {
+    handleRenameGraphTitle(id, graphTitle);
+    setNewTitle(graphTitle)
+    setOpen(false);
+    setGraphTitle(graphTitle);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+    setGraphTitle(newTitle);
+  };
+
+  const confirm = (e: React.MouseEvent<HTMLElement>) => {
+    handleDeleteGraph(id)
+    console.log(e);
+    message.success('Click on Yes');
+  };
+  
+  const cancel = (e: React.MouseEvent<HTMLElement>) => {
+    console.log(e);
+    message.error('Click on No');
   };
 
 
   return (
-    <Card size="small" title={`${title}`} extra={<Link href={`/edit/${id}`} rel="noopener noreferrer" target="_blank">View</Link>} style={{ width: 300 }}>
-      <Skeleton loading={loading} active>
-        <p>ID: {id}</p>
-        <p>Owner: {owner}</p>
-      </Skeleton>
-    </Card>
-    
+    <>
+      <Card 
+        size="small" title={`${newTitle}`} extra={<Link href={`/edit/${id}`} 
+        rel="noopener noreferrer" target="_blank">View</Link>} style={{ width: 300 }}
+        actions={[
+          <Button onClick={showModal}>Rename</Button>,
+          <Popconfirm
+            title="Delete the task"
+            description="Are you sure to delete this task?"
+            onConfirm={confirm}
+            onCancel={cancel}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button type='primary' danger>Delete</Button>
+          </Popconfirm>,
+        ]}
+      >
+        <Skeleton loading={loading} active>
+          <div>ID: {id}</div>
+          <div>Owner: {owner}</div>
+          <div>Last Modified: {new Date(lastModified.toDate()).toUTCString() || "Unknown"}</div>
+        </Skeleton>
+      </Card>
+      <Modal
+        title={"Rename Graph"}
+        open={open}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Typography.Text>Enter a new graph name:</Typography.Text>
+        <Input placeholder="New Graph Name" value={graphTitle} onChange={(e) => setGraphTitle(e.target.value)}/>
+      </Modal>
+    </>
   )
 }
 
--- pages/workspace.tsx
+++ pages/workspace.tsx
@@ -1,14 +1,17 @@
-import { collection, query, where, getDocs } from "firebase/firestore";import { useAuthState } from "react-firebase-hooks/auth";
+import { collection, query, where, getDocs, doc, serverTimestamp, setDoc, deleteDoc } from "firebase/firestore";import { useAuthState } from "react-firebase-hooks/auth";
 import GraphCard from '../components/GraphCard'
 import { auth, db } from "../utils/firebase";
 import { useEffect, useState } from 'react'
 import { IGraphInfo } from "../interfaces";
-import { Space } from "antd";
+import { Button, Input, message, Modal, Space, Typography } from "antd";
+import { v4 as uuidv4 } from 'uuid';
 
 function Workspace() : JSX.Element {
   const [loggedInUser, _loading, _error] = useAuthState(auth);
   const [graphs, setGraphs] = useState<IGraphInfo[] | any>([]);
+  const [graphTitle, setGraphTitle] = useState<string>('New Graph');
   const [loading, setLoading] = useState<boolean>(true);
+  const [open, setOpen] = useState(false);
 
   const getGraphsFromFireBase = async () : Promise<void> => {
     const q = query(collection(db, "graphs"), where("owner", "==", loggedInUser.uid));
@@ -27,26 +30,90 @@ function Workspace() : JSX.Element {
     setGraphs(output);
   }
 
+  const handleRenameGraphTitle = async (graphRenameID, graphTitle) : Promise<void> => {
+    const graphRef = doc(db,'graphs', graphRenameID);
+
+		const output = {
+			title: graphTitle,
+			lastModified: serverTimestamp(),
+		}
+		console.log('data',output)
+
+		await setDoc(graphRef, output, { merge: true });
+		message.success('Rename Successfully!');
+  }
+
+  const handleCreateGraph = async () : Promise<void> => {
+    const graphID = uuidv4();
+    const output = {
+      id: graphID,
+      title: graphTitle,
+      owner: loggedInUser.uid,
+      lastModified: serverTimestamp(),
+      graph: {
+        nodes: [],
+        links: []
+      }
+    }
+
+    const graphRef = doc(db,'graphs', graphID);
+    await setDoc(graphRef, output);
+    setGraphTitle("");
+    setGraphs([...graphs, output]);
+    message.success('Create Graph Successfully!');
+  }
+
   useEffect( () => {
     getGraphsFromFireBase();
     // reset loading
     setTimeout(() => {
       setLoading(false);
     },2000)
-
 },[]);
 
+const handleDeleteGraph = async (graphID) : Promise<void> => {
+  const graphRef = doc(db,'graphs', graphID);
+  await deleteDoc(graphRef);
+  setGraphs(graphs.filter((graph) => graph.id !== graphID));
+  message.success('Delete Graph Successfully!');
+}
+
+const showModal = () => {
+  setOpen(true);
+};
+
+const handleOk = () => {
+  handleCreateGraph();
+  setOpen(false);
+};
+
+const handleCancel = () => {
+  setOpen(false);
+  setGraphTitle("");
+};
 
   return (
     <>
       <div>Workspace</div>
+      <div>
+        <Button type="primary" onClick={showModal}>Create new graph</Button>
+      </div>
       <Space direction="vertical" size={16}>
       {
         graphs.map((graph) => (
-          <GraphCard id={graph.id} title={graph.title} owner={graph.owner} lastModified={graph.lastModified} />
+          <GraphCard id={graph.id} title={graph.title} owner={graph.owner} lastModified={graph.lastModified} handleRenameGraphTitle={handleRenameGraphTitle} handleDeleteGraph={handleDeleteGraph}/>
         ))
       }
       </Space>
+      <Modal
+        title={"Create Graph"}
+        open={open}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Typography.Text>Create a new graph name:</Typography.Text>
+        <Input placeholder="New Graph Name" value={graphTitle} onChange={(e) => setGraphTitle(e.target.value)}/>
+      </Modal>
     </>
   )
 }

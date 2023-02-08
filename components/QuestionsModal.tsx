376b1c7555f220b65e6970223a3c48354af73390
satoshiuy <lequocuyit@gmail.com>
2023-02-08 08:59:08 +0700

    update function upload to firebase

--- components/QuestionsModal.tsx
+++ components/QuestionsModal.tsx
@@ -1,7 +1,7 @@
-import { Button, Input, Modal } from 'antd'
+import { Button, Input, Modal } from 'antd';
 import { Timestamp } from 'firebase/firestore';
-import {useState, useEffect} from 'react'
-import { getQuestionFromFirebase, setQuestionToFirebase } from '../utils/firebase';
+import { useEffect, useState } from 'react';
+import { addQuestionToFirebase, getQuestionFromFirebase } from '../utils/firebase';
 
 type IProps = {
   graphID: string;
